dcb6797fe004b6658936345dced2ab8ad9fc6ecd
Muneer Ahmed Syed <muneer@ishtech.fi>
2025-11-03 17:32:25 +0200

    book table and entity improved

--- src/main/java/fi/ishtech/practice/springboot/multiport/entity/Book.java
+++ src/main/java/fi/ishtech/practice/springboot/multiport/entity/Book.java
@@ -1,5 +1,6 @@
 package fi.ishtech.practice.springboot.multiport.entity;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
@@ -12,26 +13,31 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "t_book")
+@Table(name = "t_book",
+		uniqueConstraints = @UniqueConstraint(name = "uk_book_title_author", columnNames = { "title", "author" }))
 @DynamicInsert
 @DynamicUpdate
 @Data
+@NoArgsConstructor
 public class Book implements Serializable {
 
+	@Serial
 	private static final long serialVersionUID = -4073976845697599055L;
 
 	@Id
-	@Column(updatable = false)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false, updatable = false)
 	private Long id;
 
-	@Column(nullable = false, unique = true)
+	@Column(nullable = false, unique = true, length = 255)
 	private String title;
 
-	@Column(nullable = false)
+	@Column(nullable = false, length = 255)
 	private String author;
 
 	@Column(nullable = false, precision = 4, columnDefinition = "SMALLINT")
--- src/main/resources/db/migration/V20250819_120342__create_table_book.sql
+++ src/main/resources/db/migration/V20250819_120342__create_table_book.sql
@@ -1,8 +1,8 @@
 CREATE TABLE t_book (
-  id     BIGSERIAL     NOT NULL,
-  title  VARCHAR(255)  NOT NULL UNIQUE,
+  id     BIGSERIAL     PRIMARY KEY,
+  title  VARCHAR(255)  NOT NULL,
   author VARCHAR(255)  NOT NULL,
   year   SMALLINT      NOT NULL,
   price  NUMERIC(10,2) NOT NULL,
-  PRIMARY KEY (id)
+  CONSTRAINT uk_book_title_author UNIQUE (title, author)
 );
