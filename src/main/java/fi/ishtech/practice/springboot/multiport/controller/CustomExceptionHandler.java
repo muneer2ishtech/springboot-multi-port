7484d43d2e768cca7418fa5a58ae49e17af457e1
Muneer Ahmed Syed <muneer@ishtech.fi>
2025-11-03 18:35:15 +0200

    custom exception handler

--- src/main/java/fi/ishtech/practice/springboot/multiport/controller/CustomExceptionHandler.java
+++ src/main/java/fi/ishtech/practice/springboot/multiport/controller/CustomExceptionHandler.java
@@ -1,16 +1,16 @@
 package fi.ishtech.practice.springboot.multiport.controller;
 
+import java.util.NoSuchElementException;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import fi.ishtech.practice.springboot.multiport.exception.UsernameAlreadyExistsException;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -22,24 +22,17 @@ import lombok.extern.slf4j.Slf4j;
 public class CustomExceptionHandler {
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(UsernameAlreadyExistsException.class)
-	public ResponseEntity<ErrorResponse> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException ex) {
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-				.body(ErrorResponse.create(ex, HttpStatus.BAD_REQUEST, ex.getMessage()));
-	}
-
-	@ResponseStatus(HttpStatus.UNAUTHORIZED)
-	@ExceptionHandler(BadCredentialsException.class)
-	public ResponseEntity<ErrorResponse> handleBadCredentialsException(BadCredentialsException ex) {
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-				.body(ErrorResponse.create(ex, HttpStatus.UNAUTHORIZED, ex.getMessage()));
-	}
-
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(IllegalArgumentException.class)
 	public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 				.body(ErrorResponse.create(ex, HttpStatus.BAD_REQUEST, ex.getMessage()));
+	}
+
+	@ExceptionHandler(NoSuchElementException.class)
+	public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException ex) {
+		log.error("handle NoSuchElementException", ex);
+
+		return ResponseEntity.badRequest().body(ex.getMessage());
 	}
 
 	@ExceptionHandler(DataIntegrityViolationException.class)
