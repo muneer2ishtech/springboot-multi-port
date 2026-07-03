8d615759993fbabe7f5f4578e208658ba2849708
Muneer Ahmed Syed <muneer@ishtech.fi>
2026-07-04 02:42:02 +0300

    remove unused code from test case

--- src/test/java/fi/ishtech/practice/springboot/booksapp/controller/BookRestControllerSecurityTest.java
+++ src/test/java/fi/ishtech/practice/springboot/booksapp/controller/BookRestControllerSecurityTest.java
@@ -8,10 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import fi.ishtech.practice.springboot.booksapp.service.BookService;
 import fi.ishtech.practice.springboot.multiport.MultiportCodingExerciseApplication;
 
 /**
@@ -25,9 +23,6 @@ public class BookRestControllerSecurityTest {
 
 	@Autowired
 	private MockMvc mvc;
-
-	@MockitoBean
-	private BookService bookService;
 
 	@Test
 	public void testGetAllBooksUnauthorized() throws Exception {
